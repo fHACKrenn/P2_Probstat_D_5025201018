39d25c221394ee86efa86b40e90010d736f7cce6
Fachrendy Zulfikar <89953315+fHACKrenn@users.noreply.github.com>
2022-05-29 22:19:07 +0700

    Update D_5025201018.r

--- D_5025201018.r
+++ D_5025201018.r
@@ -1,1 +1,10 @@
+# Soal 1
+before <- c(78, 75, 67, 77, 70, 72, 78, 74, 77)
+after <- c(100, 95, 70, 90, 90, 90, 89, 90, 100)
 
+difference <- after - before
+
+# Soal 2
+# Soal 3
+# Soal 4
+# Soal 5
